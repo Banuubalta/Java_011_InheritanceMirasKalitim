c0a3e3510f9eeab44c88ab75fa5552bf64baab0c
Banu balta <nbanu.balta@gmail.com>
2024-03-24 01:37:56 +0300

    Initial commit

---
+++ src/main/java/com/banubalta/departman/Muhasebeci.java
@@ -0,0 +1,23 @@
+package com.banubalta.departman;
+
+import com.banubalta.base.Personel;
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Muhasebeci extends Personel {
+    /*
+    private String adi;
+    private String soyadi;
+    private String telefonNo;
+    private String kimlikNo;
+    private String hesapNo;
+    private int personelNo;
+    private String departmani;
+    */
+    private String unvani;
+    private long sertifikaNo;
+}
